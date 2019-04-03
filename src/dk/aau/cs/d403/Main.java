7267e440f9b09a127c47d9a186deb07159e05652
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2019-04-03 15:48:40 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/dk/aau/cs/d403/Main.java
+++ src/dk/aau/cs/d403/Main.java
@@ -4,13 +4,20 @@ import dk.aau.cs.d403.parser.TestLexer;
 import dk.aau.cs.d403.parser.TestParser;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        TestLexer lexer = new TestLexer(CharStreams.fromString("Hello Bob!"));
+        TestLexer lexer = new TestLexer(CharStreams.fromString("int 23;float 1.5;"));
         TestParser parser = new TestParser(new CommonTokenStream(lexer));
 
-        String name = parser.main().name().getText();
-        System.out.println("Name: " + name);
+        //Den kan ikke lide at begge linjer
+        //String intDigit = parser.main().declarations().declaration().numberDecl().integerDecl().DIGIT().toString();
+        List<TerminalNode> floatDigit = parser.main().declarations().declarations().declaration().numberDecl().floatDecl().DIGIT();
+
+        System.out.println("integer digit: " + 0 + "\n" +
+                "float decl: " + floatDigit.get(0) + "." + floatDigit.get(1));
     }
 }
