97d78d80952a855f447a86bc6d15a67b5b89e029
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2019-04-05 16:25:25 +0200

    .spook files
    instead of .spok

--- src/dk/aau/cs/d403/Main.java
+++ src/dk/aau/cs/d403/Main.java
@@ -1,14 +1,11 @@
 package dk.aau.cs.d403;
 
-import dk.aau.cs.d403.ast.ASTnode;
 import dk.aau.cs.d403.ast.AstBuilder;
-import dk.aau.cs.d403.ast.MainNode;
 import dk.aau.cs.d403.ast.ProgramNode;
 import dk.aau.cs.d403.parser.SpookLexer;
 import dk.aau.cs.d403.parser.SpookParser;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.io.IOException;
 
@@ -16,7 +13,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            SpookLexer lexer = new SpookLexer(CharStreams.fromFileName("Resources/BasicShader.spok"));
+            SpookLexer lexer = new SpookLexer(CharStreams.fromFileName("Resources/BasicShader.spook"));
             SpookParser parser = new SpookParser(new CommonTokenStream(lexer));
 
             //ParseTree tree = parser.program();
