d69af1a2ed077852acb5fba7f5f4b91dc2f8b722
GarridoY <dgmn17@student.aau.dk>
2019-04-08 15:09:49 +0200

    Fixed bug with numbers
    Added natural numbers
    Added natural numbers to AstBuilder
    
    GUd hjÆlppp

--- src/dk/aau/cs/d403/parser/SpookParserVisitor.java
+++ src/dk/aau/cs/d403/parser/SpookParserVisitor.java
@@ -1,4 +1,4 @@
-// Generated from C:/Programming/P4/src\SpookParser.g4 by ANTLR 4.7.2
+// Generated from /Users/Garrido/PycharmProjects/PythonProjects/P4/src/SpookParser.g4 by ANTLR 4.7.2
 package dk.aau.cs.d403.parser;
 import org.antlr.v4.runtime.tree.ParseTreeVisitor;
 
@@ -160,6 +160,12 @@ public interface SpookParserVisitor<T> extends ParseTreeVisitor<T> {
 	 * @return the visitor result
 	 */
 	T visitRealNumber(SpookParser.RealNumberContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link SpookParser#naturalNumber}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitNaturalNumber(SpookParser.NaturalNumberContext ctx);
 	/**
 	 * Visit a parse tree produced by {@link SpookParser#colorFunction}.
 	 * @param ctx the parse tree
