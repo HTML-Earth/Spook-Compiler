84757b03549bc78ca7356648b9332a149e195f48
GarridoY <dgmn17@student.aau.dk>
2019-04-10 20:21:36 +0200

    Scope rule tests
    Creation of test folder
    First scope rule tests - read comments in code for more information about the tests.
    
    Also added JUnit 5.3 to Project Structure

---
+++ tests/ScopeRulesTest.java
@@ -0,0 +1,159 @@
+import dk.aau.cs.d403.ast.statements.DeclarationNode;
+import dk.aau.cs.d403.ast.statements.StatementNode;
+import dk.aau.cs.d403.ast.statements.VariableDeclarationNode;
+import dk.aau.cs.d403.ast.Enums;
+import dk.aau.cs.d403.ast.structure.*;
+import dk.aau.cs.d403.semantics.SymbolTableFilling;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ScopeRulesTest {
+    private ArrayList<StatementNode> statementNodes;
+    private ArrayList<ClassDeclarationNode> classDeclarationNodes;
+    private ArrayList<FunctionDeclarationNode> functionDeclarationNodes;
+
+    private ProgramNode programNode;
+
+    private SymbolTableFilling symbolTableFilling;
+
+    @BeforeEach
+    void setup() {
+        statementNodes = new ArrayList<>();
+        classDeclarationNodes = new ArrayList<>();
+        functionDeclarationNodes = new ArrayList<>();
+
+        symbolTableFilling = new SymbolTableFilling();
+    }
+
+    void setupNodes() {
+        BlockNode blockNode = new BlockNode(statementNodes);
+        MainNode mainNode = new MainNode(blockNode);
+        programNode = new ProgramNode(mainNode, classDeclarationNodes, functionDeclarationNodes);
+    }
+
+    @AfterEach
+    void printSymbolTable() {
+        symbolTableFilling.printSymbolTable();
+    }
+
+    // Tests if the compiler
+    // allows the declaration of variables.
+    @Test
+    void scopeRuleDeclarationTest01() {
+        // Main
+        StatementNode statementNode = new VariableDeclarationNode(Enums.DataType.INT, "var1");
+        StatementNode statementNode1 = new VariableDeclarationNode(Enums.DataType.FLOAT, "var2");
+        statementNodes.add(statementNode);
+        statementNodes.add(statementNode1);
+
+        setupNodes();
+
+        symbolTableFilling.visitProgram(programNode);
+    }
+
+    // Tests if the compiler
+    // throws an error when a variable is declared with
+    // the same name in the same scope.
+    @Test
+    void scopeRuleDeclarationTest02() {
+        // Main
+        StatementNode statementNode = new VariableDeclarationNode(Enums.DataType.INT, "var1");
+        StatementNode statementNode1 = new VariableDeclarationNode(Enums.DataType.INT, "var1");
+        statementNodes.add(statementNode);
+        statementNodes.add(statementNode1);
+
+        setupNodes();
+
+        assertThrows(RuntimeException.class, ()->{
+            symbolTableFilling.visitProgram(programNode);
+        });
+    }
+
+    // Tests if the compiler allows
+    // the declaration of variables with
+    // the same name in main scope and function scope.
+    @Test
+    void scopeRuleDeclarationTest03() {
+        // Main
+        StatementNode statementNode1 = new VariableDeclarationNode(Enums.DataType.INT, "var1");
+        statementNodes.add(statementNode1);
+
+        // New function
+        ArrayList<StatementNode> funcStatementNodes = new ArrayList<>();
+        StatementNode statementNode2 = new VariableDeclarationNode(Enums.DataType.INT, "var1");
+        funcStatementNodes.add(statementNode2);
+
+        BlockNode funcBlockNode = new BlockNode(funcStatementNodes);
+        FunctionDeclarationNode functionDeclarationNode = new FunctionDeclarationNode(Enums.ReturnType.INT, "func1", funcBlockNode);
+        functionDeclarationNodes.add(functionDeclarationNode);
+
+        setupNodes();
+
+        symbolTableFilling.visitProgram(programNode);
+    }
+
+    // Tests if the compiler allows
+    // the declaration of variables with
+    // the same name in main scope and class scope.
+    @Test
+    void scopeRuleDeclarationTest04() {
+        // Main
+        StatementNode statementNode1 = new VariableDeclarationNode(Enums.DataType.INT, "var1");
+        statementNodes.add(statementNode1);
+
+        // New class
+        ArrayList<DeclarationNode> classStatementNodes = new ArrayList<>();
+        DeclarationNode statementNode2 = new VariableDeclarationNode(Enums.DataType.INT, "var1");
+        classStatementNodes.add(statementNode2);
+
+        ClassBlockNode classBlockNode = new ClassBlockNode(classStatementNodes, functionDeclarationNodes);
+        ClassDeclarationNode classDeclarationNode = new ClassDeclarationNode("class1", classBlockNode);
+        classDeclarationNodes.add(classDeclarationNode);
+
+        setupNodes();
+        symbolTableFilling.visitProgram(programNode);
+    }
+
+    // Tests if the compiler allows
+    // the declaration of variables with
+    // the same name in main scope, function scope and function scope within class scope.
+    @Test
+    void scopeRuleDeclarationTest05() {
+        // Main
+        StatementNode statementNode1 = new VariableDeclarationNode(Enums.DataType.INT, "var1");
+        statementNodes.add(statementNode1);
+
+        // Class declarations
+        DeclarationNode declarationNode1 = new VariableDeclarationNode(Enums.DataType.INT, "var1");
+        ArrayList<DeclarationNode> declarationNodes = new ArrayList<>();
+        declarationNodes.add(declarationNode1);
+
+        // Class function block
+        StatementNode statementNode2 = new VariableDeclarationNode(Enums.DataType.INT, "var1");
+        ArrayList<StatementNode> functionStatementNodes = new ArrayList<>();
+        functionStatementNodes.add(statementNode2);
+
+        BlockNode funcBlockNode = new BlockNode(functionStatementNodes);
+
+        // Class functions
+        FunctionDeclarationNode functionNode1 = new FunctionDeclarationNode(Enums.ReturnType.VOID, "func1", funcBlockNode);
+        ArrayList<FunctionDeclarationNode> functionNodes = new ArrayList<>();
+        functionNodes.add(functionNode1);
+
+        // Class decl
+        ClassBlockNode classBlockNode = new ClassBlockNode(declarationNodes, functionNodes);
+        ClassDeclarationNode classNode = new ClassDeclarationNode("class1", classBlockNode);
+        classDeclarationNodes.add(classNode);
+
+        setupNodes();
+
+        assertThrows(RuntimeException.class, ()->{
+            symbolTableFilling.visitProgram(programNode);
+        });
+    }
+}
