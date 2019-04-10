0a74f2dafb947d8edfdb94a186e844d14b924f37
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2019-04-10 13:10:18 +0200

    Add VariableDeclarationNode and ClassBlockNode

---
+++ src/dk/aau/cs/d403/ast/structure/ClassBlockNode.java
@@ -0,0 +1,52 @@
+package dk.aau.cs.d403.ast.structure;
+
+import dk.aau.cs.d403.ast.ASTnode;
+import dk.aau.cs.d403.ast.statements.DeclarationNode;
+import dk.aau.cs.d403.ast.statements.StatementNode;
+
+import java.util.ArrayList;
+
+public class ClassBlockNode implements ASTnode {
+    private ArrayList<DeclarationNode> declarationNodes;
+    private ArrayList<FunctionDeclarationNode> functionDeclarationNodes;
+
+    public ClassBlockNode(ArrayList<DeclarationNode> declarationNodes, ArrayList<FunctionDeclarationNode> functionDeclarationNodes) {
+        this.declarationNodes = declarationNodes;
+        this.functionDeclarationNodes = functionDeclarationNodes;
+    }
+
+    public ArrayList<DeclarationNode> getDeclarationNodes() {
+        return declarationNodes;
+    }
+
+    public ArrayList<FunctionDeclarationNode> getFunctionDeclarationNodes() {
+        return functionDeclarationNodes;
+    }
+
+    @Override
+    public String prettyPrint() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("{");
+
+        for (DeclarationNode decl : declarationNodes)
+        {
+            if (decl != null) {
+                sb.append("\n\t");
+                sb.append(decl.prettyPrint());
+            }
+        }
+
+        for (FunctionDeclarationNode funcDecl : functionDeclarationNodes)
+        {
+            if (funcDecl != null) {
+                sb.append("\n\t");
+                sb.append(funcDecl.prettyPrint());
+            }
+        }
+
+        sb.append("\n}");
+
+        return sb.toString();
+    }
+}
