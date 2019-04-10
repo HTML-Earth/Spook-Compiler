2adbc8a667abba80ef182662938fda8274b013d2
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2019-04-10 15:14:16 +0200

    Print proper datatype names

--- src/dk/aau/cs/d403/ast/Enums.java
+++ src/dk/aau/cs/d403/ast/Enums.java
@@ -10,6 +10,44 @@ public class Enums {
         VEC4
     }
 
+    public static String dataTypeToStringSpook(DataType dataType) {
+        switch (dataType) {
+            case INT:
+                return "Int";
+            case FLOAT:
+                return "Float";
+            case BOOL:
+                return "Bool";
+            case VEC2:
+                return "Vec2";
+            case VEC3:
+                return "Vec3";
+            case VEC4:
+                return "Vec4";
+            default:
+                return "InvalidType";
+        }
+    }
+
+    public static String dataTypeToStringGLSL(DataType dataType) {
+        switch (dataType) {
+            case INT:
+                return "int";
+            case FLOAT:
+                return "float";
+            case BOOL:
+                return "bool";
+            case VEC2:
+                return "vector2";
+            case VEC3:
+                return "vector3";
+            case VEC4:
+                return "vector4";
+            default:
+                return "InvalidType";
+        }
+    }
+
     public enum ClassType {
         CIRCLE,
         RECTANGLE,
