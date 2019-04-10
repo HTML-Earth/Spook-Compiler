3a0432fac00fc4eb813e12c0752d2099a786d576
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2019-04-10 15:14:16 +0200

    Print proper datatype names

--- src/dk/aau/cs/d403/ast/statements/VariableDeclarationNode.java
+++ src/dk/aau/cs/d403/ast/statements/VariableDeclarationNode.java
@@ -33,8 +33,8 @@ public class VariableDeclarationNode extends DeclarationNode {
     @Override
     public String prettyPrint() {
         if (assignmentNode != null)
-            return dataType.toString() + " " + assignmentNode.prettyPrint();
+            return Enums.dataTypeToStringSpook(dataType) + " " + assignmentNode.prettyPrint();
         else
-            return dataType.toString() + " " + variableName + ";";
+            return Enums.dataTypeToStringSpook(dataType) + " " + variableName + ";";
     }
 }
