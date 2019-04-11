ed7759a40732988d9978857910f1ef4bbca4b21d
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2019-04-11 14:35:38 +0200

    Removed RectangleDeclarationNode
    And fixed object declaration node

--- src/dk/aau/cs/d403/ast/statements/ObjectDeclarationNode.java
+++ src/dk/aau/cs/d403/ast/statements/ObjectDeclarationNode.java
@@ -1,7 +1,38 @@
 package dk.aau.cs.d403.ast.statements;
 
-public class ObjectDeclarationNode extends DeclarationNode {
+import dk.aau.cs.d403.ast.Enums;
+import dk.aau.cs.d403.ast.expressions.ObjectArgumentNode;
 
+import java.util.ArrayList;
+
+public class ObjectDeclarationNode extends DeclarationNode {
+    private Enums.ClassType objectType;
+    private String variableName;
+
+    ArrayList<ObjectArgumentNode> objectArgumentNodes;
+
+    public ObjectDeclarationNode(Enums.ClassType objectType, String variableName) {
+        this.objectType = objectType;
+        this.variableName = variableName;
+    }
+
+    public ObjectDeclarationNode(Enums.ClassType objectType, String variableName, ArrayList<ObjectArgumentNode> objectArgumentNodes) {
+        this.objectType = objectType;
+        this.variableName = variableName;
+        this.objectArgumentNodes = objectArgumentNodes;
+    }
+
+    public Enums.ClassType getObjectType() {
+        return objectType;
+    }
+
+    public String getVariableName() {
+        return variableName;
+    }
+
+    public ArrayList<ObjectArgumentNode> getObjectArgumentNodes() {
+        return objectArgumentNodes;
+    }
 
     @Override
     public String prettyPrint() {
