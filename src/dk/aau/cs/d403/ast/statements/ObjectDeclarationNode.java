4d66483f75a44e1cd1a969a2b05931934d725b8f
GarridoY <dgmn17@student.aau.dk>
2019-04-11 17:02:09 +0200

    Added fields and constructor for ObjectDeclarationNode
    This is done alongside the ASTbuilder for visitObjectDecl so we could be more generic and avoid making more AST nodes such as RectangleNode.

--- src/dk/aau/cs/d403/ast/statements/ObjectDeclarationNode.java
+++ src/dk/aau/cs/d403/ast/statements/ObjectDeclarationNode.java
@@ -1,7 +1,36 @@
 package dk.aau.cs.d403.ast.statements;
 
+import dk.aau.cs.d403.ast.Enums;
+import dk.aau.cs.d403.ast.expressions.ObjectArgumentNode;
+
+import java.util.ArrayList;
+
 public class ObjectDeclarationNode extends DeclarationNode {
 
+    private Enums.ClassType classType;
+    private String variableName;
+    private ArrayList<ObjectArgumentNode> objectArguments;
+
+    public ObjectDeclarationNode(Enums.ClassType classType, String variableName, ArrayList<ObjectArgumentNode> objectArguments) {
+        this.classType = classType;
+        this.variableName = variableName;
+        this.objectArguments = objectArguments;
+    }
+
+    public ObjectDeclarationNode() {
+    }
+
+    public Enums.ClassType getClassType() {
+        return classType;
+    }
+
+    public String getVariableName() {
+        return variableName;
+    }
+
+    public ArrayList<ObjectArgumentNode> getObjectArguments() {
+        return objectArguments;
+    }
 
     @Override
     public String prettyPrint() {
