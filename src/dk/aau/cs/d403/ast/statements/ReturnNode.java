662a018e9493a442ab561012acf0de0684ecef55
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2019-04-11 14:23:13 +0200

    Added ReturnNode to AST and codegenerator

--- src/dk/aau/cs/d403/ast/statements/ReturnNode.java
+++ src/dk/aau/cs/d403/ast/statements/ReturnNode.java
@@ -1,4 +1,47 @@
 package dk.aau.cs.d403.ast.statements;
 
-public class ReturnNode {
+import dk.aau.cs.d403.ast.expressions.RealNumberNode;
+
+public class ReturnNode extends StatementNode {
+    private String variableName;
+    private RealNumberNode realNumberNode;
+    private boolean boolLiteral;
+
+    public ReturnNode(String variableName) {
+        this.variableName = variableName;
+    }
+
+    public ReturnNode(RealNumberNode realNumberNode) {
+        this.realNumberNode = realNumberNode;
+    }
+
+    public ReturnNode(boolean boolLiteral) {
+        this.boolLiteral = boolLiteral;
+    }
+
+    public String getVariableName() {
+        return variableName;
+    }
+
+    public RealNumberNode getRealNumberNode() {
+        return realNumberNode;
+    }
+
+    public boolean isBoolLiteral() {
+        return boolLiteral;
+    }
+
+    @Override
+    public String prettyPrint() {
+        String returnValue;
+
+        if (variableName != null)
+            returnValue = variableName;
+        else if (realNumberNode != null)
+            returnValue = realNumberNode.prettyPrint();
+        else
+            returnValue = "" + boolLiteral;
+
+        return "return " + returnValue + ";";
+    }
 }
