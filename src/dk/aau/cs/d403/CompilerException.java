a5c044be5829e7459f64233a7f07bdb8f9b7c466
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2019-04-12 21:11:32 +0200

    CompilerException
    can output code position
    used by AstBuilder
    fixes #16

---
+++ src/dk/aau/cs/d403/CompilerException.java
@@ -0,0 +1,14 @@
+package dk.aau.cs.d403;
+
+import dk.aau.cs.d403.ast.CodePosition;
+
+public class CompilerException extends RuntimeException {
+
+    public CompilerException(String message) {
+        super(message);
+    }
+
+    public CompilerException(String message, CodePosition codePosition) {
+        super("Line " + codePosition.toString() + " - " + message);
+    }
+}
