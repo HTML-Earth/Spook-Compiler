dbb5bd9cad1ecf14a79e479963bcb193c0ca9ece
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2019-04-15 13:09:06 +0200

    Made some Spook classes

---
+++ src/dk/aau/cs/d403/spook/shapes/Rectangle.java
@@ -0,0 +1,5 @@
+package dk.aau.cs.d403.spook.shapes;
+
+public class Rectangle extends Shape {
+
+}
---
+++ src/dk/aau/cs/d403/spook/shapes/Shape.java
@@ -0,0 +1,7 @@
+package dk.aau.cs.d403.spook.shapes;
+
+import dk.aau.cs.d403.spook.color.Colorable;
+import dk.aau.cs.d403.spook.SpookObject;
+
+public abstract class Shape extends SpookObject implements Colorable {
+}
