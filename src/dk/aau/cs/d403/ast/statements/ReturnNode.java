a0284986960a1e3fe78144fe549f6197c6a66b08
GarridoY <dgmn17@student.aau.dk>
2019-04-25 14:12:23 +0200

    ReturnNode now matches Parser

--- src/dk/aau/cs/d403/ast/statements/ReturnNode.java
+++ src/dk/aau/cs/d403/ast/statements/ReturnNode.java
@@ -5,53 +5,18 @@ import dk.aau.cs.d403.ast.expressions.ExpressionNode;
 import dk.aau.cs.d403.ast.expressions.RealNumberNode;
 
 public class ReturnNode extends StatementNode {
-    private String variableName;
-    private RealNumberNode realNumberNode;
-    private boolean boolLiteral;
     private ExpressionNode expressionNode;
-
-    public ReturnNode(String variableName) {
-        this.variableName = variableName;
-    }
-
-    public ReturnNode(RealNumberNode realNumberNode) {
-        this.realNumberNode = realNumberNode;
-    }
-
-    public ReturnNode(boolean boolLiteral) {
-        this.boolLiteral = boolLiteral;
-    }
 
     public ReturnNode(ExpressionNode expressionNode) {
         this.expressionNode = expressionNode;
     }
 
-    public String getVariableName() {
-        return variableName;
-    }
-
-    public RealNumberNode getRealNumberNode() {
-        return realNumberNode;
-    }
-
-    public boolean isBoolLiteral() {
-        return boolLiteral;
-    }
-
     @Override
     public String prettyPrint() {
-        String returnValue;
-
-        if (variableName != null)
-            returnValue = variableName;
-        else if (realNumberNode != null)
-            returnValue = realNumberNode.prettyPrint();
-        else if (expressionNode != null)
-            returnValue = expressionNode.prettyPrint();
+        if (expressionNode != null)
+            return "return " + expressionNode.prettyPrint() + ";";
         else
-            returnValue = "" + boolLiteral;
-
-        return "return " + returnValue + ";";
+            return "Invalid return statement";
     }
 
     private CodePosition codePosition;
