6548679fd26c0939d31c8db6c35d6a921a53f187
GarridoY <dgmn17@student.aau.dk>
2019-05-03 13:39:48 +0200

    Removed old method of typechecking

--- src/dk/aau/cs/d403/semantics/SymbolTable.java
+++
@@ -1,7 +0,0 @@
-package dk.aau.cs.d403.semantics;
-
-public interface SymbolTable {
-    void openScope(String name);
-    void enterSymbol(String name, NodeObject object);
-    NodeObject retrieveSymbol(String name);
-}
