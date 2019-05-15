9400e278bf44fed66313a8595e7e3b8aafff8311
Simon Mathiasen <33898979+smath17@users.noreply.github.com>
2019-05-15 11:54:06 +0200

    Testing testing 123 are natural numbers

--- tests/ExampleShadersTest.java
+++ tests/ExampleShadersTest.java
@@ -1,17 +1,18 @@
 import dk.aau.cs.d403.ast.AstBuilder;
+import dk.aau.cs.d403.ast.CodePosition;
 import dk.aau.cs.d403.ast.structure.ProgramNode;
+import dk.aau.cs.d403.codegen.CodeGenerator;
 import dk.aau.cs.d403.parser.SpookLexer;
 import dk.aau.cs.d403.parser.SpookParser;
 import dk.aau.cs.d403.semantics.TypeChecking;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-public class ExampleShadersTest {
+class ExampleShadersTest {
     private TypeChecking typeChecking = new TypeChecking();
 
     void testShader(String shaderName) {
@@ -21,8 +22,12 @@ public class ExampleShadersTest {
 
             AstBuilder astBuilder = new AstBuilder();
             ProgramNode programNode = (ProgramNode) astBuilder.visitProgram(parser.program());
+            programNode.prettyPrint(0);
 
             typeChecking.visitProgram(programNode);
+            CodeGenerator codeGenerator = new CodeGenerator();
+            codeGenerator.GenerateGLSL(programNode);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
